7c6c5447cc4010884c3b1a0b2603042494176b7a
aeropagz <51108342+aeropagz@users.noreply.github.com>
2020-09-28 16:37:01 +0200

    Update rest-api.py

--- server/backend-api/rest-api.py
+++ server/backend-api/rest-api.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
 import csv, os, datetime, json
 
-BASE_PATH = path = os.path.dirname(__file__)
+
 
 app = Flask(__name__)
 cors = CORS(app, resources = {r"/co2/*":{"origins": "*"}})
@@ -13,21 +13,21 @@ data_post_args = reqparse.RequestParser()
 data_post_args.add_argument("value", type=int)
 
 def check_if_id_exists(id):
-    if os.path.exists(BASE_PATH + "/data/" + id + ".csv"):
+    if os.path.exists("./data/" + id + ".csv"):
         return
     else:
         create_id(id)
          
 
 def create_id(id):
-    with open(BASE_PATH + "/data/" + id + ".csv", mode='w+', newline='') as csv_file:
+    with open("./data/" + id + ".csv", mode='w+', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(["time", "value"])
 
 class MeasuredValue(Resource):
     def get(self, id):
         csv = ""
-        with open(BASE_PATH + "/data/" + id + ".csv", mode='r', newline='') as csv_file:
+        with open("./data/" + id + ".csv", mode='r', newline='') as csv_file:
             for line in csv_file:
                 csv += line
 
@@ -39,7 +39,7 @@ class MeasuredValue(Resource):
         args = data_post_args.parse_args()
         check_if_id_exists(id)
         now = datetime.datetime.today().isoformat("T")
-        with open(BASE_PATH + "/data/" + id + ".csv", mode='a', newline='') as csv_file:
+        with open("./data/" + id + ".csv", mode='a', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow([now, args['value']])
 
@@ -47,7 +47,7 @@ class MeasuredValue(Resource):
 class AvaiableSensors(Resource):
     def get(self):
         files = []
-        for (_, _, filenames) in os.walk(BASE_PATH+"/data/"):
+        for (_, _, filenames) in os.walk("./data/"):
             files.extend(filenames)
             break
         for i in range(0,len(files)):
